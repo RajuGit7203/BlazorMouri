aa364561be9ae6a18db3887ac076126fdf399236
tejup <tejup@TEJU>
2024-09-02 18:38:57 -0400

    Adding XUnit Changes

---
+++ Mouri.Test/MockData/ToDoMockData.cs
@@ -0,0 +1,41 @@
+﻿using Mouri.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mouri.Test.MockData
+{
+    public class ToDoMockData
+    {
+        public static User GetUser()
+        {
+            return new User()
+            {
+                UserName = "Admin",
+                PassWord = "password"
+            };
+        }
+        public static string PatientId = "1000";
+        public static Patient GetPatient()
+        {
+            var patient = new Patient
+            {
+                Id = "1000",
+                FirstName = "Mark",
+                Lastname = "Hassan",
+                Address = new AddressInfo
+                {
+                    AddressId = 1000,
+                    City = "Rockville",
+                    State = "MD",
+                    Street = "455 Marshall",
+                    Zip = "20851"
+                },
+                Dob = Convert.ToDateTime("02/10/1985")
+            };
+            return patient;
+        }
+    }
+}
